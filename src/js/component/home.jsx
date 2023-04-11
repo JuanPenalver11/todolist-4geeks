1ad0a556f55b55f7b4d42da60b8157c15aff30a4
Juan Penalver <juanpenalversanchez11@gmail.com>
2023-04-11 18:15:57 +0000

    todolist-4geek

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,51 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import { AiOutlineDelete } from "react-icons/ai";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [input, setInput] = useState(" ");
+
+  const [task, setTask] = useState([]);
+
+  function AddTask() {
+    if (input === " ") {
+      alert("Add task");
+    } else {
+      setTask([input, ...task]);
+    }
+    setInput(" ");
+  }
+
+  function DeleteTask(index) {
+    setTask(task.filter((activity, i) => i !== index));
+  }
+
+  return (
+    <div className="container">
+      <h1>To Do List </h1>
+      <div className="addBarPlusButton">
+        <input
+          placeholder=" What you think you have to do? "
+          type="texto"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></input>
+        <button onClick={AddTask}> Add task </button>
+      </div>
+      <ul>
+        {" "}
+        {task.map((activity, index) => (
+          <li key={index}>
+            {" "}
+            {activity}
+            <AiOutlineDelete onClick={() => DeleteTask(index)} />
+          </li>
+        ))}
+        
+      </ul>
+      <div className="taskcounter"> {task.length} </div>
+    </div>
+  );
 };
 
 export default Home;
